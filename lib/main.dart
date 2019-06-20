fef7cf864654a3182e91b8cbe83f4185672ee9da
Sherlock <Farhad.public@gmail.com>
2019-06-20 10:45:09 +0430

    Create home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(HomePage());
@@ -5,6 +6,41 @@ main() => runApp(HomePage());
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return null;
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+            backgroundColor: Color(0xFFeceff1),
+            elevation: 0,
+            title: Text(
+              'data',
+              style: TextStyle(color: Colors.black),
+            ),
+            leading: IconButton(
+              iconSize: 40,
+              icon: Icon(
+                Icons.person_pin,
+                color: Colors.red,
+              ),
+              onPressed: () {},
+            ),
+            actions: <Widget>[
+              IconButton(
+                iconSize: 40,
+                icon: Icon(
+                  Icons.menu,
+                  color: Colors.grey,
+                ),
+                onPressed: () {},
+              )
+            ]),
+        body: Container(
+          child: Container(),
+          decoration: BoxDecoration(
+              color: Color(0xFFcfd8dc),
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(15), topRight: Radius.circular(15))),
+        ),
+      ),
+    );
   }
 }
